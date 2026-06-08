b259134f38b1ea5df9a5b9aafd4e08fc88525fc1
Diego <diegordz337.c@gmail.com>
2026-06-07 22:41:17 -0600

    agregar adsense

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,10 @@ export default function RootLayout({
     <html
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
+    >>
+      <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1085540329087607" crossOrigin="anonymous"></script>
+      </head>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );

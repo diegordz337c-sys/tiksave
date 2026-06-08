01a6e896e7ebf7c1852d728048587275bddf26b7
Diego <diegordz337.c@gmail.com>
2026-06-07 23:45:32 -0600

    agregar sitemap

---
+++ app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://tiksave-theta.vercel.app',
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 1,
+    },
+  ];
+}

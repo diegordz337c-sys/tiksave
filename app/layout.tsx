d245c0f46a9d46695c1dcd433a110ceced52ee4e
Diego <diegordz337.c@gmail.com>
2026-06-07 23:01:50 -0600

    agregar search console

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >>
       <head>
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1085540329087607" crossOrigin="anonymous"></script>
+     <meta name="google-site-verification" content="kp1Hml_NuGSRQCj0IaJ8hFPURf9_iEdjr9f7Jf_cVU8" />   <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1085540329087607" crossOrigin="anonymous"></script>
       </head>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>

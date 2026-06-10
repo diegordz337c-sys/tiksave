1877a86767ce915e79073cc329a9e3950dfc6b37
Diego <diegordz337.c@gmail.com>
2026-06-10 14:14:43 -0600

    agregar adsense

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -28,7 +29,13 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <head>
-     <meta name="google-site-verification" content="kp1Hml_NuGSRQCj0IaJ8hFPURf9_iEdjr9f7Jf_cVU8" />   <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1085540329087607" crossOrigin="anonymous"></script>
+        <meta name="google-site-verification" content="kp1Hml_NuGSRQCj0IaJ8hFPURf9_iEdjr9f7Jf_cVU8" />
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1085540329087607"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
       </head>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
